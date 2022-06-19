710e7d1366da46e2ab280279e3c6b3e4f551c238
Akhadafi <khadafi.dev@gmail.com>
2022-06-19 14:38:13 +0700

    05 Database Models & Admin Panel - Django Framework

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
+from .models import *
+
+
 # Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "phone",
+        "email",
+        "date_created",
+    ]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "price",
+        "category",
+        "description",
+        "date_created",
+    ]
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "date_created",
+        "status",
+    ]
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
