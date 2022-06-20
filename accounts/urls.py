9d5a1312c012d3b27bab2532f9761e26cd2904aa
Akhadafi <khadafi.dev@gmail.com>
2022-06-20 11:31:01 +0700

    09.5 Dynamic URL's - Django Framework

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="index"),
+    path("", views.home, name="home"),
     path("products", views.products, name="products"),
     path("customer/<str:pk>/", views.customer, name="customer"),
 ]
