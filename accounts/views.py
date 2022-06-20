4e0a7624e24cccd7d3090f205519f150a35d4790
Akhadafi <khadafi.dev@gmail.com>
2022-06-20 10:59:49 +0700

    08 Rendering Data to Templates  & Template Tags - Django Framework

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,39 @@
+from multiprocessing import context
+
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import *
 
 
 # Create your views here.
 def home(request):
-    return render(request, "accounts/dashboard.html")
+    # Customer
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    # Order
+    orders = Order.objects.all()
+    total_oreders = orders.count()
+    delivered = orders.filter(status="Delivered").count()
+    pending = orders.filter(status="Pending").count()
+
+    context = {
+        # Customer
+        "customers": customers,
+        "total_customers": total_customers,
+        # Order
+        "orders": orders,
+        "total_oreders": total_oreders,
+        "delivered": delivered,
+        "pending": pending,
+    }
+    return render(request, "accounts/dashboard.html", context)
 
 
 def products(request):
-    return render(request, "accounts/products.html")
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "accounts/products.html", context)
 
 
 def customer(request):
