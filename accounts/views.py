a1a91347e50fed7ccf77a2e9b9f7c6649dbb1c18
Akhadafi <khadafi.dev@gmail.com>
2022-06-20 23:36:53 +0700

    16 User Profile With One To One Relationship - Django Framework

--- accounts/views.py
+++ accounts/views.py
@@ -22,6 +22,9 @@ def registerPage(request):
             username = form.cleaned_data.get("username")
             group = Group.objects.get(name="customer")
             user.groups.add(group)
+            Customer.objects.create(
+                user=user,
+            )
             messages.success(request, "Account was created for " + username)
             return redirect("login")
     context = {
@@ -75,8 +78,20 @@ def home(request):
     return render(request, "accounts/dashboard.html", context)
 
 
+@login_required(login_url="login")
+@allowed_users(allowed_roles=["customer"])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+
+    total_oreders = orders.count()
+    delivered = orders.filter(status="Delivered").count()
+    pending = orders.filter(status="Pending").count()
+    context = {
+        "orders": orders,
+        "total_oreders": total_oreders,
+        "delivered": delivered,
+        "pending": pending,
+    }
     return render(request, "accounts/user.html", context)
 
 
