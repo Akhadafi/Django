c922b0b21ed66608324881c7ae71886b536423a5
Akhadafi <khadafi.dev@gmail.com>
2022-06-21 21:19:44 +0700

    22 File Storage with AWS S3 Buckets Upload - Django Framework

--- crm/settings.py
+++ crm/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # my apps
     "accounts.apps.AccountsConfig",
     "django_filters",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -155,3 +156,29 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "Email"
 EMAIL_HOST_PASSWORD = "Password"
+
+# S3 BUCKETS CONFIG
+"""
+AWS_ACCESS_KEY_ID = '*****************'
+AWS_SECRET_ACCESS_KEY = '*****************'
+AWS_STORAGE_BUCKET_NAME = '*****************'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+"""
+
+
+"""
+<?xml version="1.0" encoding="UTF-8"?>
+<CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+<CORSRule>
+    <AllowedOrigin>*</AllowedOrigin>
+    <AllowedMethod>GET</AllowedMethod>
+    <AllowedMethod>POST</AllowedMethod>
+    <AllowedMethod>PUT</AllowedMethod>
+    <AllowedHeader>*</AllowedHeader>
+</CORSRule>
+</CORSConfiguration>
+"""
